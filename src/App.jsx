9687787f1df440cf1b121a09982087880bd66677
ricardo <ricardo.alvear.personal@gmail.com>
2026-02-02 00:33:25 -0500

    The starting structure of the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from './components/layouts/Mainlayout/main-layout.jsx'
+import Home from './components/pages/home/homepage.jsx'
+
+function App() {
+  return (
+      <div></div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/file-tracking-progress/file-tracking-progress.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function FileTrackingProgress() {
+  return (
+    <div>
+      <h1>File Tracking Progress</h1>
+    </div>
+  );
+}
+
+export default FileTrackingProgress;
